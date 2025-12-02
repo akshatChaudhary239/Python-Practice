7aac2bf01b61ef4cb8fbb98242c79649b8142fc4
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-02 13:31:24 +0530

    "Done"

--- DAY-2 TASKs/string_basics.py
+++ DAY-2 TASKs/string_basics.py
@@ -1,12 +1,15 @@
 # String basic recaller
 
 Name= input("enter your name ")
+no_space = Name.replace(" ","")
+char_count = len(no_space)
 first_initial = Name[0]
 second_initial = Name[-1]
-middle_name = Name[1:-2:1]
+middle_name = Name[1:-1]
 resversed_name= Name[::-1]
 print(f'''
-Your full name: {len(Name)}
+Your full name: {Name}
+Actual Length of the Name : {char_count} 
 First Initial: {first_initial}
 Last Initial: {second_initial}
 Middle Name: {middle_name}
